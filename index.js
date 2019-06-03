5b756e998df095c4058e85fd9d9d6ef73dbb2e28
Elsiph <Elsiph>
2019-06-02 17:54:39 -0700

    renamed groupwork, added modal to index.html and functionality to style.css and index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 //index.js
+
+var create = document.getElementById('createbutton');
+var accept = document.getElementById('acceptbutton');
+var cancel = document.getElementById('cancelbutton');
+var scrollname = document.getElementById('newscrollname');
+var modal = document.getElementById('modal');
+var backdrop = document.getElementById('modalbackdrop');
+
+//display modal
+create.addEventListener('click',function(event){
+  modal.classList.remove('hidden');
+  backdrop.classList.remove('hidden');
+});
+
+cancel.addEventListener('click',function(event){
+  modal.classList.add('hidden');
+  backdrop.classList.add('hidden');
+  scrollname.value = "";
+});
+
+accept.addEventListener('click',function(event){
+  modal.classList.add('hidden');
+  backdrop.classList.add('hidden');
+  scrollname.value = "";
+  //send signal to server to create a new empty scroll page
+  //redirect to 'edit' page for this scroll
+});
